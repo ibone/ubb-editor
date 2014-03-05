dab6a254fbf5712fe436d66be7389d0895eb4890
ibone <water2212682@gmail.com>
2014-03-05 14:58:15 +0800

    'update'

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,20 +25,18 @@ module.exports = function (grunt) {
             },
             dist: {
                 src: [
-                    'src/intro.js',
-                    'src/core.js',
-                    'src/key_map.js',
-                    'src/paste.js',
-                    'src/ubb_to_html.js',
-                    'src/html_to_ubb.js',
-                    'src/outro.js',
-                    'src/font.js',
-                    'src/ubb_map.js',
-                    'src/btn_bold.js',
-                    'src/btn_color.js',
-                    'src/btn_size.js',
-                    'src/btn_link.js',
-                    'src/btn_split.js'
+                    'src/core/intro.js',
+                    'src/core/core.js',
+                    'src/core/key_map.js',
+                    'src/core/paste.js',
+                    'src/core/ubb_to_html.js',
+                    'src/core/html_to_ubb.js',
+                    'src/core/outro.js',
+                    'src/toolbar/btn_bold.js',
+                    'src/toolbar/btn_color.js',
+                    'src/toolbar/btn_size.js',
+                    'src/toolbar/btn_link.js',
+                    'src/toolbar/btn_split.js'
                 ],
                 dest: 'dist/ubb_editor.js'
             }
